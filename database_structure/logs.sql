bd0ed898ecea63a0c3ccd94135d248bd0a8682ea
Jason Olimpio <olimpiojason@gmail.com>
2024-03-12 00:10:34 +0100

    Update database_structure folder

--- database_structure/logs.sql
+++ database_structure/logs.sql
@@ -27,7 +27,7 @@ CREATE TABLE `logs` (
   `message` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
   `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=241 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=242 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -39,4 +39,4 @@ CREATE TABLE `logs` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-02-29 22:17:42
+-- Dump completed on 2024-03-12  0:06:12
